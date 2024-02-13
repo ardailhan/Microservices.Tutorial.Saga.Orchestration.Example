999b8af5f15f973fa6a591e53db07a634b942b88
Arda İlhan <ardailhan7@gmail.com>
2024-02-13 14:49:46 +0300

    Order,Payment,Stock events configured

---
+++ Shared/OrderEvents/OrderCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.OrderEvents
+{
+    public class OrderCreatedEvent : CorrelatedBy<Guid>
+    {
+        public OrderCreatedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+        public Guid CorrelationId { get; }
+    }
+}
